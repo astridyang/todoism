715faa6a9ffdbcc3e5b77932801387a1d26caab9
sallyyoo <workyooll@gmail.com>
2018-12-06 11:51:09 +0800

    index template

--- todoserver/todoism/blueprints/mission.py
+++ todoserver/todoism/blueprints/mission.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, render_template, current_app, request
 from flask_login import login_required
-from ..models import Category
+from ..models import Category, Plan
 mission_bp = Blueprint('mission', __name__)
 
 
 @mission_bp.route('/')
 @login_required
 def index():
-    return render_template('mission/index.html')
+    plan_list = Plan.query.all()
+
+    return render_template('mission/index.html', plan_list=plan_list)
